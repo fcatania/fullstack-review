f0e8ad1567a2a63f1ca6140a6023db81cead697d
Felipe Catania <felicatania@gmail.com>
2018-01-02 20:15:21 -0800

    Server using cors and bodyParser middleware. Missing actual github helper call.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
-let app = express();
 const { save, Repo } = require('../database/index.js');
+const { getReposByUsername } = require('../helpers/github.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+let app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 
-// Allowing CORS. Can also use 'cors' middleware.
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
-app.post('/repos', function (req, res) {
+app.use(cors());
+
+app.post('/repos', bodyParser.json(), function (req, res) {
   // TODO: Get repos from github API.
-  // console.log(req.body); // TODO: require and npm install bodby parser middleware to easy get body.
-                         // OR: send the term as /repos/:term and use req.params.term or sth like it.
+  console.log(req.body.username);
+  // getReposByUsername(req.body.username);
+  // TO BE CONTINUED...
 });
 
 app.get('/repos', function (req, res) { // TODO: Sort the TOP 25, however I want.
