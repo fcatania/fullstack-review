00a6804893a018b14875108492b5e9d071d61df4
Felipe Catania <felicatania@gmail.com>
2018-01-02 17:42:15 -0800

    Implemented requests structure in client.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,12 +10,30 @@ class App extends React.Component {
     this.state = { 
       repos: []
     }
-
+  }
+  
+  componentDidMount() {
+    fetch('http://127.0.0.1:1128/repos').then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error('Error occurred while fetching (GET).')
+      }
+    }).then(json => {
+      console.log(json);
+    });
   }
 
   search (term) {
     console.log(`${term} was searched`);
-    // TODO
+    fetch('http://127.0.0.1:1128/repos', {method: 'POST', body: term, mode: 'cors'}).then(reponse => {
+      console.log(reponse);
+      if (reponse.ok) {
+        console.log('OK!');
+      } else {
+        console.error('Error occurred while fetching (POST).');
+      }
+    });
   }
 
   render () {
